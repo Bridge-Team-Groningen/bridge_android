902090497e79ee0fb111e65dc563729b5d064478
Tigran <tigran.kocharyan@outlook.com>
2022-03-19 15:43:28 +0300

    Add profile page and edit profile page

--- app/src/main/java/nl/totowka/bridge/presentation/LauncherActivity.kt
+++ app/src/main/java/nl/totowka/bridge/presentation/LauncherActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import nl.totowka.bridge.R
+import nl.totowka.bridge.presentation.auth.view.AuthFragment
 
 /**
  * Main Activity. All fragments will be added on top of it.
@@ -13,7 +14,14 @@ class LauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val splash = installSplashScreen()
+        installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, AuthFragment.newInstance(), AuthFragment.TAG)
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/nl/totowka/bridge/presentation/profile/view/details/ProfileFragment.kt
@@ -0,0 +1,68 @@
+package nl.totowka.bridge.presentation.profile.view.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import nl.totowka.bridge.databinding.FragmentProfileBinding
+import androidx.appcompat.app.AppCompatActivity
+import nl.totowka.bridge.R
+import nl.totowka.bridge.presentation.auth.view.AuthFragment
+import nl.totowka.bridge.presentation.profile.view.edit.EditProfileFragment
+
+
+/**
+ * Фрагмент, отвечающий за экран изучения слов.
+ */
+class ProfileFragment : Fragment(), View.OnClickListener {
+    private lateinit var binding: FragmentProfileBinding
+    var account: GoogleSignInAccount? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        account = arguments?.getParcelable(PROFILE_TAG)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.editProfile.setOnClickListener(this)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity?)?.supportActionBar?.hide()
+    }
+
+    override fun onClick(view: View?) {
+        if (view != null) {
+            when (view.id) {
+                R.id.edit_profile -> (activity as AppCompatActivity).supportFragmentManager
+                    .beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.fragment_container, EditProfileFragment.newInstance(), EditProfileFragment.TAG)
+                    .commit()
+            }
+        }
+    }
+
+    companion object {
+        const val TAG = "ProfileFragment"
+        const val PROFILE_TAG = "PROFILE_TAG"
+        private const val TAG_ADD = "$TAG ADD"
+        private const val TAG_ERROR = "$TAG ERROR"
+        private const val TAG_PROGRESS = "$TAG PROGRESS"
+
+        /**
+         * Получение объекта [ProfileFragment]
+         */
+        fun newInstance(account: GoogleSignInAccount) = ProfileFragment().apply {
+            arguments = Bundle().apply {
+                putParcelable(PROFILE_TAG, account)
+            }
+        }
+    }
+}
